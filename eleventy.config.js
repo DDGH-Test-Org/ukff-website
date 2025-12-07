e26aa5b13c01b0c5fc136d0eda3777d8e5050cf2
David Dorward <david@dorward.me.uk>
2025-12-07 17:56:09 +0000

    Remove prefix for using subdomain

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,9 +4,9 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addGlobalData("layout", "default-layout.njk");
   eleventyConfig.setOutputDirectory("docs");
-  eleventyConfig.addPlugin(HtmlBasePlugin);
+  // eleventyConfig.addPlugin(HtmlBasePlugin);
 }
 
-export const config = {
-  pathPrefix: "/ukff-website/",
-};
+// export const config = {
+//   pathPrefix: "/ukff-website/",
+// };
