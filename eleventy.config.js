03af4db2746183dab264bc3222f2979243231b3a
David Dorward <david@dorward.me.uk>
2025-12-07 17:16:29 +0000

    Prefix URLs for test Github Pages URL

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,12 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addGlobalData("layout", "default-layout.njk");
   eleventyConfig.setOutputDirectory("docs");
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 }
+
+export const config = {
+  pathPrefix: "/ukff-website/",
+};
